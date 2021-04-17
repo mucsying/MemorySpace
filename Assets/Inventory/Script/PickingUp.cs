19bd2fa4f9fca5fb136a10cc530a19d42f9c7a16
XuChenHua <xutysky@outlook.com>
2021-04-17 16:33:57 +0800

    add inventory system and basic folder

---
+++ Assets/Inventory/Script/PickingUp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Inventory
+{
+	public class PickingUp : MonoBehaviour
+	{
+		private BagManager _bagManager;
+
+		private void Start()
+		{
+			_bagManager = BagManager.Instance;
+		}
+
+		public void PickUp(string itemName)
+		{
+			_bagManager.PutInBag(itemName);
+		}
+
+		private void OnMouseDown()
+		{
+			PickUp(name);
+		}
+	}
+}
